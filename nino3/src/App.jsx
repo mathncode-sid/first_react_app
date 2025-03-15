dd957fe80878fc31eee0c7f47b39d5c239991b42
mathncode-sid <sidneybarakamuriuki1@gmail.com>
2025-03-15 14:28:46 +0300

    First Application in React

---
+++ nino3/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/navbar'
+
+function App() {
+  return(
+    <>
+    <Navbar/>
+    <Navbar/>
+    <h1>maincomponent</h1>
+    </>
+  )
+}
+
+export default App
---
+++ nino3/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import '../App.css'
+
+function Navbar() {
+
+  return (
+    <>
+      <div className='container'>
+        <div><h1>Logo</h1></div>
+        <div>React</div>
+        <div>
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+              <li>Project</li>
+            </ul>
+          </nav>
+        </div>
+        </div> 
+    </>
+  )
+}
+
+export default Navbar
